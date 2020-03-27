6a2384652e6cc21dc4de49440b89d9526613e8a1
Reetro <thegamerboy7@gmail.com>
2020-03-27 13:02:47 -0600

    Setup the combat text widget

---
+++ Plugins/EasyCombatText/Source/EasyCombatText/Private/CombatTextWidget.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CombatTextWidget.h"
+
+void UCombatTextWidget::SetCombatText(FText Text)
+{
+  CombatText = Text;
+}
---
+++ Plugins/EasyCombatText/Source/EasyCombatText/Public/CombatTextWidget.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CombatTextWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EASYCOMBATTEXT_API UCombatTextWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+  UPROPERTY(BlueprintReadOnly, Category = "Combat Text Vars")
+  FText CombatText;
+
+  UFUNCTION(BlueprintCallable, Category = "Combat Text Functions")
+  void SetCombatText(FText Text);
+
+};
